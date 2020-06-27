7abfbedfdc33ec79d94699fdc4e322d14d5bc3dc
Matt_Bundner <mbundner13@gmail.com>
2020-06-27 16:11:23 -0400

    defined arrays for cards

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("GAME OF WAR!")
+//GAME OF WAR
+
+const cardFace = ["2 of ","3 of ","4 of ","5 of ","6 of ","7 of ","8 of ","9 of ","10 of ","jack of ","queen of ","king of ", "ace of "];
+const cardSuits = ["spades", "clubs", "diamonds", "hearts"];
+const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+const deck = [];
+const players = [[],[]];
