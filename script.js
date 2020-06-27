58fedbbf728399993b6aafbd2bb171419e367f03
Matt_Bundner <mbundner13@gmail.com>
2020-06-27 18:46:25 -0400

    added function  for 2 player deal

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const cardSuits = ["spades", "clubs", "diamonds", "hearts"];
 const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 const deck = [];
-const players = [[],[]];
+const player1 = [];
+const player2 = [];
 
 for (let i=0; i<cardFace.length; i++){
     for (let j=0; j<cardSuits.length; j++){
@@ -27,4 +28,15 @@ for (let i=0; i<deck.length; i++){
   deck[randomIndex] = x;
 }
 
-console.log(deck)
+//console.log(deck)
+
+//code for dealing the deck to 2 players
+for (i=0; i<deck.length; i++){
+  if (i%2 !== 0){
+    player1.push(deck[i])
+  }else{
+    player2.push(deck[i])
+  }
+}
+console.log(player1)
+console.log(player2)
