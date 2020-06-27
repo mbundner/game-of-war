dc535f391066ce05d651696430135cb439d3d9ae
Matt_Bundner <mbundner13@gmail.com>
2020-06-27 16:36:44 -0400

    used nested array to make deck

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 const deck = [];
 const players = [[],[]];
+
+for (let i=0; i<cardFace.length; i++){
+    for (let j=0; j<cardSuits.length; j++){
+        let card = {
+          face: cardFace[i],
+          suit: cardSuits[j],
+          value: cardValue[i]
+        }
+        deck.push(card)
+    }
+  }
+console.log(deck)
